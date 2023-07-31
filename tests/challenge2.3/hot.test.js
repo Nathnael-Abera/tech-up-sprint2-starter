da66ce375e343cc2b853fa0b8d5050fd8e19e452
Yosef Samuel <yosefsamuel22@gmail.com>
2023-07-31 15:13:22 +0300

    New tests added

--- tests/challenge2.3/hot.test.js
+++ tests/challenge2.3/hot.test.js
@@ -5,17 +5,19 @@ import { Job, Candidate, Skill } from '../../common/model';
 const j1 = new Job('J1', '', [new Skill('s1', 0), new Skill('s2', 2), new Skill('s3', 2)], 'M');
 const j2 = new Job('J2', '', [new Skill('s1', 0), new Skill('s2', 2), new Skill('s3', 2)], 'F');
 const j3 = new Job('J3', '', [new Skill('s1', 0), new Skill('s2', 2), new Skill('s3', 2)]);
+const j4 = new Job('J4', '', [new Skill('s1', 2), new Skill('s2', 2), new Skill('s3', 3), new Skill('s4', 2), new Skill('s5', 3)]);
 
-const c1 = new Candidate('C', null, [[new Skill('s3', 0), new Skill('s2', 2), new Skill('s1', 2)]], 'M');
-const c2 = new Candidate('C', null, [[new Skill('s3', 2), new Skill('s2', 2), new Skill('s1', 2)]], 'M');
-const c3 = new Candidate('C', null, [[new Skill('s4', 2), new Skill('s4', 2)]], 'M');
+const c1 = new Candidate('C', null, [new Skill('s3', 0), new Skill('s2', 2), new Skill('s1', 2)], 'M');
+const c2 = new Candidate('C', null, [new Skill('s3', 2), new Skill('s2', 2), new Skill('s1', 2)], 'M');
+const c3 = new Candidate('C', null, [new Skill('s4', 2), new Skill('s3', 1)], 'M');
+const c4 = new Candidate('C', null, [new Skill('s1', 2), new Skill('S2', 2), new Skill ('s3', 2), new Skill('s4', 2), new Skill('S5', 3)], 'F');
 
 describe('Hotness Tests', () => {
   test('Hotness test', () => {
     expect(
       Utils.hottestCandidate([c1], [j1, j2, j3]))
       .toBe(
-        2
+        0
       );
   });
 
@@ -23,7 +25,7 @@ describe('Hotness Tests', () => {
     expect(
       Utils.hottestCandidate([c1, c2], [j1, j2, j3]))
       .toBe(
-        3
+        2
       );
   });
 
@@ -31,7 +33,7 @@ describe('Hotness Tests', () => {
     expect(
       Utils.hottestCandidate([c1, c2, c3], [j1, j2, j3]))
       .toBe(
-        3
+        2
       );
   });
 
@@ -39,15 +41,15 @@ describe('Hotness Tests', () => {
     expect(
       Utils.hottestCandidate([c1, c3], [j1, j2, j3]))
       .toBe(
-        2
+        0
       );
   });
 
   test('Hotness test', () => {
     expect(
-      Utils.hottestCandidate([c3], [j1, j2, j3]))
+      Utils.hottestCandidate([c2,c4], [j3,j4]))
       .toBe(
-        0
+        2
       );
   });
 });
