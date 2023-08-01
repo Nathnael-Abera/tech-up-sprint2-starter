42f1bc713352f1e451ea961e10e3f4a9696a237c
Yosef Samuel <yosefsamuel22@gmail.com>
2023-08-01 11:46:04 +0300

    Update skills-match.test.js

--- tests/challenge2.3/skills-match.test.js
+++ tests/challenge2.3/skills-match.test.js
@@ -43,7 +43,7 @@ test('Skills match test', () => {
   expect(
     Utils.skillsMatch(s3, s4))
     .toBe(
-      false
+      true
     );
 });
 
@@ -51,6 +51,6 @@ test('Skills match test', () => {
   expect(
     Utils.skillsMatch(s4, s3))
     .toBe(
-      true
+      false
     );
 });
